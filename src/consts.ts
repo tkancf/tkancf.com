02a66983de838ae776a7af4a7276135d0dfbbce0
tkancf <18424095+tkancf@users.noreply.github.com>
2023-09-17 22:17:07 +0900

    Update profile

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "tkanのブログ";
-export const SITE_DESCRIPTION = "tkanのブログです。";
+export const SITE_TITLE = "tkan's blog";
+export const SITE_DESCRIPTION =
+  "tkanのブログです。私が気になったものについてジャンル問わず何でも予定です。";
