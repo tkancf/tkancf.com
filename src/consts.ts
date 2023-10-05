21c40fc3a148afae7c1b641372cc0adadec7af63
tkancf <18424095+tkancf@users.noreply.github.com>
2023-10-05 15:44:52 +0900

    Update site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "tkan's blog";
+export const SITE_TITLE = "tkancf.com";
 export const SITE_DESCRIPTION =
-  "tkanのブログです。私が気になったものについてジャンル問わず何でも予定です。";
+  "ブログです。私が気になったものについてジャンル問わず何でも書く予定です。";
