aee9a98a5b435729a4891b400f1d6dfabb44b2f8
tkancf <18424095+tkancf@users.noreply.github.com>
2024-03-02 17:54:17 +0900

    Fix twitter image

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -19,14 +19,12 @@ export const Head: FC = (props) => {
       <meta property="og:description" content={props.metadata.description} />
       <meta property="og:image" content={props.metadata.ogImage} />
       {/* Twitter Card */}
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={props.metadata.url} />
-      <meta property="twitter:title" content={props.metadata.title} />
-      <meta
-        property="twitter:description"
-        content={props.metadata.description}
-      />
-      <meta property="twitter:image" content={props.metadata.ogImage} />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@tkancf" />
+      <meta name="twitter:creator" content="@tkancf" />
+      <meta name="twitter:title" content={props.metadata.title} />
+      <meta name="twitter:description" content={props.metadata.description} />
+      <meta name="twitter:image" content="/twitter-card-image.jpg" />
     </head>
   );
 };
