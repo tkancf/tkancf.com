ae2c0b321d491592115342f7df0e78de5d2f9c76
tkancf <18424095+tkancf@users.noreply.github.com>
2024-03-10 22:11:07 +0900

    Fix to live reload
    Change library vite-plugin-live-reload to vite-plugin-full-reload

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,24 @@ import { defineConfig } from "vite";
 import ssg from "@hono/vite-ssg";
 import devServer from "@hono/vite-dev-server";
 import Sitemap from "vite-plugin-sitemap";
+import FullReload from "vite-plugin-full-reload";
 import { baseURL } from "./src/lib/constants";
-import liveReload from "vite-plugin-live-reload";
 
 const entry = "src/index.tsx";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === "development";
+
+  console.log("isDevelopment", isDevelopment);
+  if (isDevelopment) {
+    return {
+      plugins: [devServer({ entry }), FullReload("./content/(*|*/*.md)")],
+    };
+  }
   return {
     plugins: [
-      devServer({ entry }),
       ssg({ entry }),
       Sitemap({ hostname: baseURL, generateRobotsTxt: true }),
-      liveReload("./content/(*|*/*.md)"),
     ],
   };
 });
