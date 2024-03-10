ea4a38a9263d0474d47bc52f82051bcd516640ca
tkancf <18424095+tkancf@users.noreply.github.com>
2024-03-10 21:49:28 +0900

    Added live reload support for markdown files in content directory to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import ssg from "@hono/vite-ssg";
 import devServer from "@hono/vite-dev-server";
 import Sitemap from "vite-plugin-sitemap";
 import { baseURL } from "./src/lib/constants";
+import liveReload from "vite-plugin-live-reload";
 
 const entry = "src/index.tsx";
 
@@ -12,6 +13,7 @@ export default defineConfig(() => {
       devServer({ entry }),
       ssg({ entry }),
       Sitemap({ hostname: baseURL, generateRobotsTxt: true }),
+      liveReload("./content/(*|*/*.md)"),
     ],
   };
 });
