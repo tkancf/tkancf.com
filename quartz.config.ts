d8eee2b86130bfe02009561df4eebec9b3d47c5e
tkancf <18424095+tkancf@users.noreply.github.com>
2025-09-01 22:52:17 +0900

    rssFullHtml to true
    > rssFullHtml: If true, the RSS feed includes full HTML content. Otherwise it includes just summaries.
    https://quartz.jzhao.xyz/plugins/ContentIndex

--- quartz.config.ts
+++ quartz.config.ts
@@ -82,6 +82,7 @@ const config: QuartzConfig = {
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: true,
+        rssFullHtml: true,
       }),
       Plugin.Assets(),
       Plugin.Static(),
